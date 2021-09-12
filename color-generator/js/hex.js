f3feb290f2023521ae1b970f8ad35aba932f6574
Alex <shura.tabolich@mail.ru>
2021-09-12 18:43:09 +0300

    Resolve review comments

--- color-generator/js/hex.js
+++ color-generator/js/hex.js
@@ -1,19 +1,24 @@
 const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"],
     mixColorsBtn = document.querySelector('#btn'),
-    main = document.querySelector('main'),
-    colorName = document.querySelector('.name');
+    mainTag = document.querySelector('main'),
+    colorName = document.querySelector('.name'),
+    burgerBtn = document.querySelector('#burger'),
+    mobileMenu = document.querySelector('.menu-container');
 
 
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('burger-active');
+    mobileMenu.classList.toggle('show-menu');
+});
 mixColorsBtn.addEventListener('click', () => {
     let hex = '';
-    const getRandomColorHex = (arr) => {
-        for (let i = 0; i < 6; i++) {
-            const randomNumber = Math.floor(Math.random() * arr.length);
-            hex += hexArr[randomNumber];
-        }
-        main.style.background = `#${hex}`;
-        colorName.textContent = `#${hex}`;
-    };
-    getRandomColorHex(hexArr);
+
+    for (let i = 0; i < 6; i++) {
+        const randomNumber = Math.floor(Math.random() * hexArr.length);
+        hex += hexArr[randomNumber];
+    }
+    mainTag.style.background = `#${hex}`;
+    colorName.textContent = `#${hex}`;
+
 
 });
--- color-generator/js/index.js
+++ color-generator/js/index.js
@@ -1,18 +1,18 @@
 const burgerBtn = document.querySelector('#burger'),
     mobileMenu = document.querySelector('.menu-container'),
-    mixColorsBtn = document.querySelector('#btn'),
+    btnGenerateRandomColor = document.querySelector('#btn'),
     colorsArr = ['red', 'orange', 'yellow', 'green', 'aqua', 'blue', 'purple'],
-    main = document.querySelector('main'),
+    mainTag = document.querySelector('main'),
     colorName = document.querySelector('.name');
 
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger-active');
     mobileMenu.classList.toggle('show-menu');
 });
-mixColorsBtn.addEventListener('click', () => {
+btnGenerateRandomColor.addEventListener('click', () => {
     const getRandomColor = (arr) => {
         const randomNumber = Math.floor(Math.random() * arr.length);
-        main.style.background = colorsArr[randomNumber];
+        mainTag.style.background = colorsArr[randomNumber];
         colorName.textContent = colorsArr[randomNumber];
     };
     getRandomColor(colorsArr);
