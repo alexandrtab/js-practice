8449a33474ee4b39d7e043c16821146e7995d179
Alex <shura.tabolich@mail.ru>
2021-09-14 21:41:11 +0300

    Edit by review

--- tabs/js/index.js
+++ tabs/js/index.js
@@ -2,27 +2,29 @@ const tabNavButtons = document.querySelectorAll(".btn"),
     tabImages = document.querySelectorAll('.img'),
     tabTxts = document.querySelectorAll('.tab-txt');
 
+const showActiveBtnHandler = (arr, item) => {
+    arr.forEach((item) => {
+        item.classList.remove('active');
+    });
+    item.classList.toggle('active');
+
+};
+
+const showActiveContentHandler = (arr, index) => {
+    arr.forEach((item) => {
+        item.classList.remove('show');
+    });
+    arr[index].classList.toggle('show');
+};
+
+
 tabNavButtons.forEach((item, index) => {
 
     item.addEventListener('click', () => {
-        const showActiveButtonHandle = (arr) => {
-            arr.forEach((item) => {
-                item.classList.remove('active');
-            });
-            item.classList.toggle('active');
 
-        };
-
-        const showActiveContentHandle = (arr) => {
-            arr.forEach((item) => {
-                item.classList.remove('show');
-            });
-            arr[index].classList.toggle('show');
-        };
-
-        showActiveButtonHandle(tabNavButtons);
-        showActiveContentHandle(tabTxts);
-        showActiveContentHandle(tabImages);
+        showActiveBtnHandler(tabNavButtons, item);
+        showActiveContentHandler(tabTxts, index);
+        showActiveContentHandler(tabImages, index);
     });
 
 });
